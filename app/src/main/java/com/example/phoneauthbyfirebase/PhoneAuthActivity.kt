3f1c98e5e89aaeef42233591e0b05eec0ec62099
Dev_dominos <masury2011@gmail.com>
2020-12-11 01:35:33 +0900

    first commit

---
+++ app/src/main/java/com/example/phoneauthbyfirebase/PhoneAuthActivity.kt
@@ -0,0 +1,15 @@
+package com.example.phoneauthbyfirebase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PhoneAuthActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_phone_auth)
+    }
+
+
+}
