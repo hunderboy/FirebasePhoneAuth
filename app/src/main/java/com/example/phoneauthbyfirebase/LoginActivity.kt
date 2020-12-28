e16c5f8c3104ae92c4dc0f2a3763d7df731f188c
Dev_dominos <masury2011@gmail.com>
2020-12-28 23:35:33 +0900

    // 자동로그인 준비중

--- app/src/main/java/com/example/phoneauthbyfirebase/LoginActivity.kt
+++ app/src/main/java/com/example/phoneauthbyfirebase/LoginActivity.kt
@@ -1,26 +1,21 @@
 package com.example.phoneauthbyfirebase
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.phoneauthbyfirebase.databinding.ActivityLoginBinding
 import com.example.phoneauthbyfirebase.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view) // 뷰 바인딩 적용 완료
 
-
-        // 로그인
-        binding.buttonLogin.setOnClickListener {
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-        }
-
+    
     }
 
 
